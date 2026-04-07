3debb5a98cbb3ed856738350fe835de387bc50d4
Max Arne Olofsson <maxarne.olofsson@learnet.se>
2026-04-07 12:49:44 +0200

    init

--- m4l01/src/lesson-steps/01-simple-components/components/02-hello-world.js
+++ m4l01/src/lesson-steps/01-simple-components/components/02-hello-world.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import { uniqueId, seedGenerator } from '../../../services/seido-helpers';
+import React from "react";
+import { uniqueId, seedGenerator } from "../../../services/seido-helpers";
 
 //Making Hello World into a component
 function HelloWorld02() {
-
-  const greeting = "Good morning";
-  const address = {city: "Oslo", country: "Norway"};
+  const Greeting = "Good morning";
+  const address = { city: "Oslo", country: "Norway" };
   const paragraph = <p>My special paragraph</p>;
-  
+
   const _seeder = new seedGenerator();
   const q = _seeder.quote;
 
   return (
     <div>
-    <h1>{_seeder.latinSentence} A function component</h1>
-    <h1 id="my-heading">
-      text before span tag:&nbsp;
-      <span>
-        hello&nbsp;<em>Wonderful&nbsp;</em>World
-      </span>
-      &nbsp;:text after span tag 
-    </h1>
-    <p>I live in {address.city} which is in {address.country}</p>
-    {paragraph}
-    <p>{q.quote}</p>
+      <h1>{_seeder.latinSentence} A function component</h1>
+      <h1 id="my-heading">
+        text before span tag:&nbsp;
+        <span>
+          hello&nbsp;<em>Wonderful&nbsp;</em>World
+        </span>
+        &nbsp;:text after span tag
+      </h1>
+      <p>
+        I live in {address.city} which is in {address.country}
+      </p>
+      {paragraph}
+      <p>{q.quote}</p>
     </div>
-    
+
     /*
     React.createElement(
     'h1',
@@ -40,7 +41,7 @@ function HelloWorld02() {
      ' :text after span tag',
    )
    */
-  )
+  );
 }
 
-export default HelloWorld02
+export default HelloWorld02;
